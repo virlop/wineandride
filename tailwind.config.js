3051e19e31137c30ac8618ae264f76cdad98f4ba
Virginia Lopez <virginialopez@Virginias-MacBook-Pro.local>
2025-01-09 21:46:24 +0100

    agrego index portada

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  prefix: 'tw-', // Prefijo para las clases de Tailwind
+  content: [
+    './*.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
